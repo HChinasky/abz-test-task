21b65b46b009f866edb491cb612b5d6be67c8547
v01d <yarik_watson@hotmail.com>
2022-05-13 12:22:52 +0300

    added IndexPage added app style added normalize.css

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <IndexPage />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import IndexPage from "@/pages/IndexPage";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    IndexPage
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "scss/app.scss";
 </style>
